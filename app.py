5a74db4c140b12c830829a64682abaff45e23e88
tsung-han <d5269357812>
2021-04-15 05:57:53 +0800

    initial commit for we lunch app , start dev from here 四月十五 清晨六點

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask import request
-import json
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
@@ -14,43 +13,6 @@ db = MongoEngine(app)
 
 import mongoengine as me
 
-class Course(me.Document):
-    NAME = me.StringField(required=True)
-    meta = {'collection': 'COURSE',"strict":False }
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return render_template('hi.html', name=username)
-
-
-@app.route("/course",methods=['GET'])
-def getCourse():
-    data = Course.objects()
-    return jsonify([json.loads(ele.to_json()) for ele in data])
-
-@app.route("/course/<name>",methods=['GET'])
-def getCourseByName(name):
-    data = Course.objects(NAME = name)
-    return jsonify([json.loads(ele.to_json()) for ele in data])
-@app.route("/course",methods=['PUT'])
-def putCourse():
-    newC = Course(NAME = request.form["NAME"])
-    newC.save()
-    return json.loads(newC.to_json())
-@app.route("/course",methods=['POST'])
-def postCourse():
-    course = Course.objects().get(id = request.form["_id"])
-    # TODO how to handle muiltiple column?
-    data = request.form
-    for key, value in data.items():
-        if(key[0]!="_"):course[key]=value
-    course.save()
-    return json.loads(course.to_json())
-@app.route("/course",methods=['DELETE'])
-def delCourse():
-    Course.objects().get(id = request.form["_id"]).delete()
-    return "del success"
 @app.route("/webhook",methods=['POST'])
 def webhook():
     client = LineBotApi("jpkaZF4J41V3hTOT7kinpR16tTormHg0pKDpr5UJX6sOyeIETiHnXOYveDupNa6Zk6KKE1B+zZSiKQJ8qSrGVCeDD2EEsRzXeOEImKtQfrU1UjvLysgAvcRoGpMVos79emD+gZT3uJvF1O2pLJIQkgdB04t89/1O/w1cDnyilFU=")
--- test.py
+++ test.py
@@ -20,38 +20,5 @@ class TestFunction(unittest.TestCase):
         for obj in app.Course.objects():
             obj.delete()
 
-    def test_courseC(self):
-        newcourse = app.Course(NAME="性愛吹吹吹")
-        response = self.client.put('/course',data=newcourse.to_mongo() )
-        result = app.Course.objects(NAME = "性愛吹吹吹")
-        self.assertEqual(response.status_code,200)
-        self.assertIsNotNone(result)
-    def test_courseR(self):
-        app.Course(NAME="讀龍課程").save()
-        app.Course(NAME="讀龍鑽研課程").save()
-        response  = self.client.get('/course')
-        allcourse = app.Course.objects()
-        self.assertEqual(response.status_code, 200)
-        for course in allcourse:
-            courseName = course.to_mongo()["NAME"]
-            isIn = False
-            for ele in response.json:
-                if ele["NAME"] == courseName:isIn = True
-            self.assertTrue(isIn)
-
-    def test_courseD(self):
-        delcourse = app.Course(NAME = "刪掉我")
-        delcourse.save()
-        response = self.client.delete('/course',data = delcourse.to_mongo())
-        self.assertEqual(response.status_code, 200)
-        result = app.Course.objects(NAME = "幹幹幹")
-        self.assertFalse(result)
-    def test_courseU(self):
-        sbbb = app.Course.objects().first()
-        sbbb["NAME"]="壞妹妹教學"
-        response = self.client.post('/course',data = sbbb.to_mongo())
-        self.assertEqual(response.status_code, 200)
-        result = app.Course.objects(NAME = "壞妹妹教學")
-        self.assertTrue(result)
 if __name__ == '__main__':
     unittest.main()
