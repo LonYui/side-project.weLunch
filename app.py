6b7e36323164b545163fd8561dd5fbe54ecbb752
tsung-han <d5269357812>
2021-04-15 03:18:54 +0800

    feature: line echo bot

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_mongoengine import MongoEngine
 from flask import request
 import json
 
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
     "host":"mongodb+srv://Tsung:d39105648@cluster0.dmjou.mongodb.net/Cluster0?retryWrites=true&w=majority"
@@ -48,6 +51,14 @@ def postCourse():
 def delCourse():
     Course.objects().get(id = request.form["_id"]).delete()
     return "del success"
+@app.route("/webhook",methods=['POST'])
+def webhook():
+    client = LineBotApi("jpkaZF4J41V3hTOT7kinpR16tTormHg0pKDpr5UJX6sOyeIETiHnXOYveDupNa6Zk6KKE1B+zZSiKQJ8qSrGVCeDD2EEsRzXeOEImKtQfrU1UjvLysgAvcRoGpMVos79emD+gZT3uJvF1O2pLJIQkgdB04t89/1O/w1cDnyilFU=")
+    rJson = request.json
+    for event in rJson["events"]:
+        client.reply_message(event["replyToken"],TextSendMessage(text=event["message"]["text"]))
+    return "ok"
+
 
 if __name__ == '__main__':
     app.run()
