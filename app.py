79f8d54831a7bfcdfd35f2b512c4ffdbe162f596
tsung-han <d5269357812>
2021-04-19 04:32:57 +0800

    bug:更新完 test 後沒過 因為status 沒有initial cluster 的 field 沒有 initial 成功 refactor:新增 final annotation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+from typing import Final
 import cluster
 app = Flask(__name__)
 
@@ -11,19 +12,19 @@ def webhook():
     client = LineBotApi("jpkaZF4J41V3hTOT7kinpR16tTormHg0pKDpr5UJX6sOyeIETiHnXOYveDupNa6Zk6KKE1B+zZSiKQJ8qSrGVCeDD2EEsRzXeOEImKtQfrU1UjvLysgAvcRoGpMVos79emD+gZT3uJvF1O2pLJIQkgdB04t89/1O/w1cDnyilFU=")
     rJson = request.json["events"]
     if not rJson:return "ok"
-    userId = rJson[0]["source"]["userId"]
-    user = cluster.getUser(userId)
+    userId:Final = rJson[0]["source"]["userId"]
+    user:Final = cluster.getUser(userId)
     # status 1 ~ 2
     if not user:
-        reqstext = rJson[0]["message"]["text"]
-        token = rJson[0]["replyToken"]
+        reqstext:Final = rJson[0]["message"]["text"]
+        token:Final = rJson[0]["replyToken"]
         replytext =""
         if reqstext == "開始使用":replytext = "請輸入性別"
         elif reqstext in ["男生","男"]:
-            cluster.Male(userId=userId).save()
+            cluster.Male(userId=userId,status=3).save()
             replytext = "注意：需用<>標記訊息"
         elif reqstext in ["女生","女"]:
-            cluster.Female(userId = userId).save()
+            cluster.Female(status=3,userId = userId).save()
             replytext = "注意：需用<>標記訊息"
         if token!=userId:client.reply_message(token,TextSendMessage(text=replytext))
         return replytext
--- cluster.py
+++ cluster.py
@@ -4,31 +4,31 @@ from mongoengine import connect
 connect(host ="mongodb+srv://Tsung:d39105648@restaurant.m9bx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" )
 
 class Male(me.Document):
-    nickName = me.StringField
-    birthDate = me.DateField
-    personality = me.StringField
+    nickName = me.StringField()
+    birthDate = me.DateField()
+    personality = me.StringField()
     hobit = me.StringField(max_length=20)
-    job = me.StringField
-    pictUri = me.URLField
+    job = me.StringField()
+    pictUri = me.URLField()
     email = me.StringField(regex="""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
-    phone = me.StringField
+    phone = me.StringField()
 
     userId = me.StringField(unique=True)
-    status = me.IntField
+    status = me.IntField()
     meta = {'collection': 'Male'}
 
 class Female(me.Document):
-    nickName = me.StringField
-    birthDate = me.DateField
-    personality = me.StringField
+    nickName = me.StringField()
+    birthDate = me.DateField()
+    personality = me.StringField()
     hobit = me.StringField(max_length=20)
-    job = me.StringField
-    pictUri = me.URLField
+    job = me.StringField()
+    pictUri = me.URLField()
     email = me.StringField(regex="""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
-    phone = me.StringField
+    phone = me.StringField()
 
     userId = me.StringField(unique=True)
-    status = me.IntField
+    status = me.IntField()
     meta = {'collection': 'Female'}
 
 def getUser(userId):
