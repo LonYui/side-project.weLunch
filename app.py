4225aa829f3917e0c7c3d52e7a43aea3273e882b
tsung-han <d5269357812>
2021-05-02 17:26:46 +0800

    style: sove pylint & type probleam funvtion doc,ordering import,consistent function return

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
-from re import search 
+from re import search
 from datetime import date as dt
+from typing import Final
+from flask import Flask, request
 from linebot import LineBotApi
 from linebot.models import TextSendMessage, template, actions
-from typing import Final
 import cluster
 
 app = Flask(__name__)
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def webhook():
+    """line api webhook"""
     client = LineBotApi("""
         jpkaZF4J41V3hTOT7kinpR16tTormHg0pKDpr5UJX6sOy
         eIETiHnXOYveDupNa6Zk6KKE1B+zZSiKQJ8qSrGVCeDD2
@@ -19,41 +20,38 @@ def webhook():
         """)
     rJson = request.json["events"]
     if not rJson:
-        return "ok"
-    Etype: Final = rJson[0]["type"]
-    if Etype not in ["postback", "message"]:
+        return None
+    eventType: Final = rJson[0]["type"]
+    if eventType not in ["postback", "message"]:
         return
     userId: Final = rJson[0]["source"]["userId"]
     user = cluster.getUser(userId)
     token: Final = rJson[0]["replyToken"]
-    replytext = ""
-    if Etype == "postback":
+    replyT = ""
+    if eventType == "postback":
         status = user.status
         if status == 100:
-            reqstext: Final = rJson[0]["postback"]["data"]
+            reqsT: Final = rJson[0]["postback"]["data"]
             user.status += 10
-            date = cluster.getDate(reqstext)
-            replytext = date.ST_Dstatus(reqstext=userId,
-                                        userId=userId,
-                                        token=token, client=client)
+            date = cluster.getDate(reqsT)
+            replyT = date.ST_Dstatus(reqsT=userId, userId=userId,
+                                     token=token, client=client)
             user.save()
-            return replytext
         elif status == 110:
-            reqstext: Final = rJson[0]["postback"]["data"]
+            reqsT: Final = rJson[0]["postback"]["data"]
             date = cluster.getDate(userId)
-            replytext = date.ST_Dstatus(reqstext=reqstext,
-                                        userId=userId,
-                                        token=token, client=client)
-            return replytext
+            replyT = date.ST_Dstatus(reqsT=reqsT, userId=userId,
+                                     token=token, client=client)
+        return replyT
 
     # status 1 ~ 2
-    reqstext: Final = rJson[0]["message"]["text"]
+    reqsT: Final = rJson[0]["message"]["text"]
     if not user:
-        if reqstext in ["開始使用", "去註冊約她"]:
-            replytext = "請輸入性別"
+        if reqsT in ["開始使用", "去註冊約她"]:
+            replyT = "請輸入性別"
             action1 = actions.MessageAction(text="男生", label="男生")
             action2 = actions.MessageAction(text="女生", label="女生")
-            column = template.CarouselColumn(text=replytext,
+            column = template.CarouselColumn(text=replyT,
                                              actions=[action1, action2])
             carouse = template.CarouselTemplate(columns=[column])
             if token != userId:
@@ -61,9 +59,9 @@ def webhook():
                     token,
                     [template.TemplateSendMessage(template=carouse,
                                                   alt_text="broke")])
-            return replytext
-        elif reqstext == "預覽約會":
-            replytext = "本週約會有"
+            return replyT
+        elif reqsT == "預覽約會":
+            replyT = "本週約會有"
             action = actions.MessageAction(text="去註冊約她", label="去註冊約她")
             columnL = []
             for girl in cluster.Female.objects(status__gte=15):
@@ -83,40 +81,40 @@ def webhook():
             carouse = template.CarouselTemplate(columns=columnL)
             if token != userId:
                 client.reply_message(
-                    token, [TextSendMessage(text=replytext),
+                    token, [TextSendMessage(text=replyT),
                             template.TemplateSendMessage(
                                 template=carouse,
                                 alt_text="broke")])
-            return replytext
-        elif reqstext in ["男生", "男"]:
+            return replyT
+        elif reqsT in ["男生", "男"]:
             cluster.Male(userId=userId, status=3).save()
-            replytext = "注意：需用<>標記訊息"
-        elif reqstext in ["女生", "女"]:
+            replyT = "注意：需用<>標記訊息"
+        elif reqsT in ["女生", "女"]:
             cluster.Female(status=3, userId=userId).save()
-            replytext = "注意：需用<>標記訊息"
+            replyT = "注意：需用<>標記訊息"
         else:
-            replytext = "歡迎"
+            replyT = "歡迎"
             action1 = actions.MessageAction(text="開始使用", label="開始使用")
             action2 = actions.MessageAction(text="預覽約會", label="預覽約會")
             column = template.CarouselColumn(
-                text=replytext, actions=[action1, action2])
+                text=replyT, actions=[action1, action2])
             carouse = template.CarouselTemplate(columns=[column])
             if token != userId:
                 client.reply_message(
                     token, [template.TemplateSendMessage(
                         template=carouse,
                         alt_text="broke")])
-            return replytext
+            return replyT
         if token != userId:
-            client.reply_message(token, TextSendMessage(text=replytext))
-        return replytext
+            client.reply_message(token, TextSendMessage(text=replyT))
+        return replyT
     status = user.status
     # status 3 ~ 15
     if status == 3:
         user.status += 1
-        replytext = "請輸入<名字>"
+        replyT = "請輸入<名字>"
     elif status == 4:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -124,9 +122,9 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.nickName = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<生日> (yyyy-mm-dd)"
+        replyT = "請輸入<生日> (yyyy-mm-dd)"
     elif status == 5:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -134,45 +132,45 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.birthDate = dt.fromisoformat(m.groups()[0])
         user.status += 1
-        replytext = "您是" + user.birthDate.isoformat() \
-                    + "的" + cluster.getConstellation(
+        replyT = "您是" + user.birthDate.isoformat() \
+                 + "的" + cluster.getConstellation(
             user.birthDate.month, user.birthDate.day)
         if user.__class__.__name__ == "Male":
-            replytext += "男孩嗎？"
+            replyT += "男孩嗎？"
         elif user.__class__.__name__ == "Female":
-            replytext += "女孩嗎？"
+            replyT += "女孩嗎？"
         action1 = actions.MessageAction(text="沒錯", label="沒錯")
         action2 = actions.MessageAction(text="剛剛手滑了", label="剛剛手滑了")
         column = template.CarouselColumn(
-            text=replytext, actions=[action1, action2])
+            text=replyT, actions=[action1, action2])
         carouse = template.CarouselTemplate(columns=[column])
         if token != userId:
             client.reply_message(
                 token, [template.TemplateSendMessage(template=carouse,
                                                      alt_text="broke")])
         user.save()
-        return replytext
+        return replyT
 
     elif status == 6:
-        if reqstext in ["是", "沒錯", "y"]:
+        if reqsT in ["是", "沒錯", "y"]:
             user.status += 1
-            replytext = "請輸入<個人特質>"
-        elif reqstext in ["不是", "剛剛手滑了", "n"]:
+            replyT = "請輸入<個人特質>"
+        elif reqsT in ["不是", "剛剛手滑了", "n"]:
             user.status -= 1
             user.birthDate = None
-            replytext = "請輸入<生日> (yyyy-mm-dd)"
+            replyT = "請輸入<生日> (yyyy-mm-dd)"
     elif status == 7:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
-            if token != userId: 
-                client.reply_message(
-                    token, TextSendMessage(text="偵測不到<>，請再試一次"))
+            if token != userId:
+                client.reply_message(token, TextSendMessage(
+                    text="偵測不到<>，請再試一次"))
             return "偵測不到<>，請再試一次"
         user.personality = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<興趣>"
+        replyT = "請輸入<興趣>"
     elif status == 8:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -180,9 +178,9 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.hobit = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<職業>"
+        replyT = "請輸入<職業>"
     elif status == 9:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -190,9 +188,9 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.job = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<照片url>"
+        replyT = "請輸入<照片url>"
     elif status == 10:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -200,9 +198,9 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.pictUri = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<信箱>"
+        replyT = "請輸入<信箱>"
     elif status == 11:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -210,9 +208,9 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.email = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<電話>"
+        replyT = "請輸入<電話>"
     elif status == 12:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         if not m:
             if token != userId: 
                 client.reply_message(
@@ -220,17 +218,17 @@ def webhook():
             return "偵測不到<>，請再試一次"
         user.phone = m.groups()[0]
         user.status += 1
-        replytext = "請輸入<驗證碼>，查看手機簡訊"
+        replyT = "請輸入<驗證碼>，查看手機簡訊"
     elif status == 13:
-        m = search("<(\\S+)>", reqstext)
+        m = search("<(\\S+)>", reqsT)
         validatCode = m.groups()[0]
         if validatCode != "iampassword":
             user.phone = None
             user.status -= 1
-            replytext = "錯誤，請再輸入一次手機"
+            replyT = "錯誤，請再輸入一次手機"
         else:
             user.status += 1
-            replytext = "最後確認，這樣資料正確嗎？"
+            replyT = "最後確認，這樣資料正確嗎？"
             text = "個性" + user.personality \
                    + "喜歡" + user.hobit + "的" + \
                    cluster.getConstellation(
@@ -246,22 +244,22 @@ def webhook():
             carouse = template.CarouselTemplate(columns=[column])
             if token != userId:
                 client.reply_message(
-                    token, [TextSendMessage(text=replytext),
+                    token, [TextSendMessage(text=replyT),
                             template.TemplateSendMessage(
                                 template=carouse, alt_text="broke")])
             user.save()
-            return replytext
+            return replyT
     elif status == 14:
-        if reqstext in ["是", "沒錯", "y", "確認"]:
+        if reqsT in ["是", "沒錯", "y", "確認"]:
             user.status += 1
-            replytext = "待審核後就可以開始使用了"
+            replyT = "待審核後就可以開始使用了"
 
     elif status == 100:
-        if reqstext == "發起約會":
-            replytext = "上班在哪一區呀？"
+        if reqsT == "發起約會":
+            replyT = "上班在哪一區呀？"
             cluster.Date(femaleId=user.userId, status=1).save()
             user.status += 10
-        elif reqstext == "觀看約會":
+        elif reqsT == "觀看約會":
             colLis = []
             for dating in cluster.Date.objects(status == 10):
                 girl = cluster.getUser(dating.femaleId)
@@ -282,10 +280,10 @@ def webhook():
                 client.reply_message(
                     token, [template.TemplateSendMessage(
                         template=carouse, alt_text="broke")])
-            return replytext
+            return replyT
     elif status == 110:
         date = cluster.getDate(userId)
-        if reqstext == "觀看邀請名單":
+        if reqsT == "觀看邀請名單":
             colLis = []
             for invId in date.invList:
                 user = cluster.getUser(invId)
@@ -309,15 +307,15 @@ def webhook():
                 client.reply_message(
                     token, [template.TemplateSendMessage(
                         template=carouse, alt_text="broke")])
-            return ""
-        replytext = date.ST_Dstatus(
-            reqstext=reqstext, userId=userId,
+            return None
+        replyT = date.ST_Dstatus(
+            reqsT=reqsT, userId=userId,
             token=token, client=client)
-        return replytext
+        return replyT
     user.save()
     if token != userId:
-        client.reply_message(token, TextSendMessage(text=replytext))
-    return replytext
+        client.reply_message(token, TextSendMessage(text=replyT))
+    return replyT
 
 
 if __name__ == '__main__':
